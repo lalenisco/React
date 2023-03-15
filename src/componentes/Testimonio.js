4febe2fa3e3f6fcd3797979afc31eb7f1cc775aa
Leonardo Lenis <leonardo.lenis@masglobalconsulting.com>
2023-03-15 17:29:55 -0500

    Primeros pasos del proyecto

---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,24 @@
+import React from "react";
+import '../hojas-de-estilo/Testimonio.css'
+
+function Testimonio() {
+  return (
+    <div className="contenedor-testimonio">
+      <img
+        className="imagen-testimonio"
+        src={require("../imagenes/Emma.png")}
+        alt="Foto Emma"
+      />
+      <div className="contenedor-texto-testimonio">
+        <p className="nombre-testimonio">Emma Bostian en Suecia</p>
+        <p className="cargo-testimonio">Ingeniero de software en Spotify</p>
+        <p className="texto-testimonio">
+          {" "}
+          "Siempre he tenido problemas para aprender JavaScript. Tomé muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como un ingeniero de software en Spotify".
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonio;
