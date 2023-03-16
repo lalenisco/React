bc83ded0267bcd4eee9f0252a7528ab6108d4038
Lalenisco <lalensico91@gmail.com>
2023-03-16 08:26:27 -0500

    Se agregan los props

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,21 +1,18 @@
-import React from "react";
+import React from 'react';
 import '../hojas-de-estilo/Testimonio.css'
 
-function Testimonio() {
+function Testimonio(props) {
   return (
-    <div className="contenedor-testimonio">
+    <div className='contenedor-testimonio'>
       <img
-        className="imagen-testimonio"
-        src={require("../imagenes/Emma.png")}
-        alt="Foto Emma"
+        className='imagen-testimonio'
+        src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+        alt='Foto Emma'
       />
-      <div className="contenedor-texto-testimonio">
-        <p className="nombre-testimonio">Emma Bostian en Suecia</p>
-        <p className="cargo-testimonio">Ingeniero de software en Spotify</p>
-        <p className="texto-testimonio">
-          {" "}
-          "Siempre he tenido problemas para aprender JavaScript. Tomé muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como un ingeniero de software en Spotify".
-        </p>
+      <div className='contenedor-texto-testimonio'>
+        <p className='nombre-testimonio'>{props.nombre} en {props.pais}</p>
+        <p className='cargo-testimonio'>{props.cargo} en {props.empresa}</p>
+        <p className='texto-testimonio'>"{props.testimonio}"</p>
       </div>
     </div>
   );
